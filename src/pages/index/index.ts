5cb61ddfda6e7c0c2a54c3cccd0e8d40956e1d1b
Dmitry <neox56@gmail.com>
2022-08-22 20:46:04 +0400

    Add reviews-section

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,17 +1,40 @@
-import {getComponents, getComponent} from "@/helpers/helpers";
-import {ITransitionData} from "@barba/core/dist/core/src/defs";
+import { getComponents, getComponent } from "@/helpers/helpers";
+import { ITransitionData } from "@barba/core/dist/core/src/defs";
+import Swiper, { Navigation } from "swiper";
 
 export default {
     namespace: "common",
-    async beforeEnter({next}: ITransitionData) {
+    async beforeEnter({ next }: ITransitionData) {
         try {
+            // Инициализация компонентов
+
+            new Swiper(".swiper", {
+                modules: [Navigation],
+                navigation: {
+                    nextEl: ".slider__btn_next",
+                    prevEl: ".slider__btn_prev"
+                },
+                on: {
+                    slideChange: function(swiper) {
+                        const counterValue = swiper.$wrapperEl
+                            .children()
+                            [swiper.activeIndex].querySelector(
+                            ".counter__value"
+                        )?.textContent;
+                    }
+                }
+            });
+
         } catch (e) {
-            console.error(e);
+            // @ts-ignore
+            sliderItems.forEach((item) => item.destroy());
         }
     },
     beforeLeave() {
+        // @ts-ignore
+        sliderItems.forEach((item) => item.destroy());
     },
 
     afterLeave() {
-    },
+    }
 };
